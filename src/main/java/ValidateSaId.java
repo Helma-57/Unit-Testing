920b353d48e9fb8cdb9d81456732b5c455c3b7b6
Helma <makhubelehelma@gmail.com>
2025-04-23 15:19:52 +0200

    adding public class

--- src/main/java/ValidateSaId.java
+++ src/main/java/ValidateSaId.java
@@ -4,3 +4,13 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
+
+public class ValidateSaId {
+
+    public static boolean isIdNumberValid(String idNumber) {
+        return isProperLengthAndDigits(idNumber)
+                && isValidDate(idNumber.substring(0, 6))
+                && isValidCitizenship(idNumber.charAt(10))
+                && isValidLuhn(idNumber);
+    }
+
